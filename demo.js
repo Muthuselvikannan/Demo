00f960bd7b75a8e63f8ecbf5c919bc490c7a5642
Muthuselvikannan <muthuselvitamizh@gmail.com>
2024-02-20 21:21:27 +0530

    git push

---
+++ demo.js
@@ -0,0 +1,13 @@
+
+
+//num = 10;
+
+//console.log(num)
+
+let kidsName = {Name1: "Varun", Name2: "Sakthi", Name3: "Sarvesh"};
+
+
+console.log(kidsName.Name1);
+console.log(kidsName.Name2)
+console.log(kidsName.Name3)
+console.log(kidsName["Name1"])
